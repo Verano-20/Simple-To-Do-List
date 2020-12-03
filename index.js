3385ba99d699c82f1b0160ac95f3ad268b93795b
Verano-20 <samdawson97@hotmail.co.uk>
2020-12-03 16:50:05 +0000

    Converted to jQuery

--- index.js
+++ index.js
@@ -1,32 +1,25 @@
-var input = document.getElementById("task-input")
-var submitbtn = document.getElementById("submit-btn")
-var list = document.getElementById("task-list")
-
-submitbtn.addEventListener('click', function(e) {
-    var firstTask = list.firstElementChild
-    var listItem = document.createElement('li')
-    var newTask = document.createElement('p')
-    var textNode = document.createTextNode(input.value)
-    var binIcon = document.createElement('span')
-
-    listItem.classList.add('task-item')
-    newTask.appendChild(textNode)
-    listItem.appendChild(newTask)
-    listItem.appendChild(binIcon)
-    listItem.addEventListener('click', function(){
-        if (listItem.classList.contains("completed")){
-            listItem.classList.remove("completed")
-        } else {
-            listItem.classList.add("completed")
-        }})
-
-    list.insertBefore(listItem, firstTask)
-
-    binIcon.addEventListener('click', function() {
-        console.log('delete')
-        list.removeChild(listItem)
+$(document).ready(() => {
+    var input = $("#task-input")
+    $("#submit-btn").on('click', (e) => { 
+        var listItem = $("<li></li>", {
+            html: `<p>${input.val()}</p>`,
+            "class": "task-item",
+            on: {
+                'click': function() {
+                    $(this).toggleClass('completed')
+                }
+            }
+        })
+        var binIcon = $("<span></span>", {
+            on: {
+                'click': function() {
+                    $(this).parent().remove()
+                }
+            }
+        })
+        binIcon.appendTo(listItem)
+        listItem.prependTo($("#task-list"))        
+        e.preventDefault()
+        input.val('')
     })
-
-    e.preventDefault()
-    input.value=''
 })
